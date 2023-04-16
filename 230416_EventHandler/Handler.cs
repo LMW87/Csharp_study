b212c07f2cc0884846469e47482388fe4b901d96
Myungwoo Lee <dlauddn123@gmail.com>
2023-04-16 15:51:37 +0900

    최초 빌드수행

---
+++ 230416_EventHandler/Handler.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    private void Calculate(int a)
+    {
+        a *= 2;
+    }
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+
+        int val = 100;
+        p.Calculate(val);
+        // val는 그대로 100        
+    }
+}
